ef1bf60b9edf85985ce3c564521740fab8300d64
chelle <maphaiyarath@gmail.com>
2021-02-12 00:53:12 -0600

    got the seeds to actually seed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,73 @@
-const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const db = require("../models/");
+
+module.exports = function(app) {
+
+    // get api workouts
+	app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    // post api workouts
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create(body).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    // get api workouts range
+
+    // put api workouts id
 
 /*
-router.post("/api/transaction", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+    app.post("/submit", ({body}, res) => {
+        db.Book.create(body).then(({_id}) => {
+            db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true });
+        }).then(dbLibrary => {
+            res.json(dbLibrary);
+        }).catch(err => {
+            res.json(err);
+        });
     });
-});
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+    app.get("/api/workouts/:id", (req, res) => {
+        Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
     });
-});
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+    app.get("/api/workouts/range", (req, res) => {
+        Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
     });
-});
-*/
 
-module.exports = router;
+    app.get("/exercise/:id", (req, res) => {
+        Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+    */
+
+    /*
+  app.put("/api/images/:id", function(req, res) {
+    db.Image.updateOne(
+      { _id: req.params.id },
+      { rating: req.body.rating }
+    ).then(function(dbImage) {
+      res.json(dbImage);
+    });
+  });
+    */
+};
